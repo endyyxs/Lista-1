fb4c3b8b7c85e18df7e8e8f7c522d92d61ce1673
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-09-19 16:23:30 -0300

    Atualizar o ex_5.c

--- Lista_3/ex_5.c
+++ Lista_3/ex_5.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int n = 8;  // Número de termos da sequência de Fibonacci
-    int fib[n]; // Array para armazenar os termos
+    int n = 8;  
+    int fib[n]; 
 
-    // Os dois primeiros termos são fixos
     fib[0] = 0;
     fib[1] = 1;
 
-    // Calculando os próximos termos da sequência
     for (int i = 2; i < n; i++) {
         fib[i] = fib[i-1] + fib[i-2];
     }
 
-    // Exibindo os termos
     printf("Os oito primeiros termos da sequência de Fibonacci são:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", fib[i]);
