8e09f23ea21e9ea1a6c89845b37ab33e20c826c8
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-09-19 18:55:39 -0300

    Lista_2

---
+++ Lista_2/ex_14.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    float sal, bonif, aux_esc, novosal;
+
+    printf("Digite o salario do funcionario: ");
+    scanf("%f", &sal);
+
+    if (sal <= 500.00) {
+        bonif = sal * 0.05; 
+    } else if (sal <= 1200.00) {
+        bonif = sal * 0.12; 
+    } else {
+        bonif = 0.00; 
+    }
+
+    if (sal <= 600.00) {
+        aux_esc = 150.00; 
+    } else {
+        aux_esc = 100.00; 
+    }
+
+    novosal = sal + bonif + aux_esc;
+
+    printf("O novo salario, incluindo bonificacao e auxilio escola e: R$ %.2f\n", novosal);
+
+    return 0;
+}
